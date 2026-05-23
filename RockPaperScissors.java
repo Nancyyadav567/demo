ef80a5bebafb15afcafd33b7f3bda58ee3a42739
Nancyyadav567 <nancy07yadav06@gmail.com>
2026-05-23 22:12:26 +0530

    RockPaperScissors.java
    Uploaded RockPaperScissors.java project. This game allows the user to play Rock, Paper, Scissors against the computer using Java.

---
+++ RockPaperScissors.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class RockPaperScissors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] choices = {"Rock", "Paper", "Scissors"};
+
+        System.out.println("=== Rock Paper Scissors Game ===");
+
+        System.out.print("Enter your choice (Rock, Paper, Scissors): ");
+        String userChoice = sc.nextLine();
+
+        int computerIndex = random.nextInt(3);
+        String computerChoice = choices[computerIndex];
+
+        System.out.println("Computer chose: " + computerChoice);
+
+        if (userChoice.equalsIgnoreCase(computerChoice)) {
+            System.out.println("It's a Draw!");
+        } else if (
+                (userChoice.equalsIgnoreCase("Rock") && computerChoice.equals("Scissors")) ||
+                (userChoice.equalsIgnoreCase("Paper") && computerChoice.equals("Rock")) ||
+                (userChoice.equalsIgnoreCase("Scissors") && computerChoice.equals("Paper"))
+        ) {
+            System.out.println("You Win!");
+        } else {
+            System.out.println("Computer Wins!");
+        }
+
+        sc.close();
+    }
+}
